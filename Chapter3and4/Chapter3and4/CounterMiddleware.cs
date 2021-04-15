a5e4bafc04244b259e3cc1c6ba646d5be2e471a4
Ihor Kolopatin <ikolopatin@gmail.com>
2021-04-15 18:50:23 +0300

    here is chapter 3

---
+++ Chapter3and4/Chapter3and4/CounterMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chapter3and4.Services;
+namespace Chapter3and4
+{
+    public class CounterMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private int i = 0;
+       // TimeService _timeService;
+        public CounterMiddleware(RequestDelegate next,TimeService timeService)
+        {
+            _next = next;
+         //   _timeService = timeService;
+        }
+        public async Task Invoke(HttpContext context, CounterService cs, ICounter c, TimeService _timeService)
+        {
+           
+                //context.Response.ContentType = "text/html; charset=utf-8";
+                context.Response.ContentType = "text/html;charset=utf-8";
+                await context.Response.WriteAsync($"Запрос {i}; Counter: {c.Value}; Service: {cs.Counter.Value};time: {_timeService?.Time}");
+           
+        }
+    }
+}
