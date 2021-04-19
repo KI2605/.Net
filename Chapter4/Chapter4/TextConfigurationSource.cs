9614cdd55014af7bd9dfa6e39f0625b649c55855
Ihor Kolopatin <ikolopatin@gmail.com>
2021-04-19 19:08:51 +0300

    here is chapter 4 and 5

---
+++ Chapter4/Chapter4/TextConfigurationSource.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter4
+{
+    public class TextConfigurationSource : IConfigurationSource
+    {
+        public string FilePath { get; private set; }
+        public TextConfigurationSource(string path)
+        {
+            FilePath = path;
+        }
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            string fpath = builder.GetFileProvider().GetFileInfo(FilePath).PhysicalPath;
+            return new TextConfigurationProvider(fpath);
+        }
+    }
+}
