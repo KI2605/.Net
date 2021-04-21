bebebf5781369f1b1c517d69180735f0ea97b409
Ihor Kolopatin <ikolopatin@gmail.com>
2021-04-21 12:29:59 +0300

    here is chapter8

---
+++ Chapter8/Chapter8/Models/MobileContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter8.Models
+{
+    public class MobileContext : DbContext
+    {
+        // свойства DBset соотносятся с таблицами в базах данных
+        
+        //получаем набор обьектов Phone
+        public DbSet<Phone> Phones { get; set; }
+
+        //получаем набор обьектов Order
+        public DbSet<Order> Orders { get; set; }
+        public MobileContext(DbContextOptions<MobileContext> options) : base (options)
+        {
+            // по умолчанию база данных отсутствует, создаем ее, если она уже есть, ничего не произойдет
+            Database.EnsureCreated();
+
+        }
+    }
+}
---
+++ Chapter8/Chapter8/SampleData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chapter8.Models;
+namespace Chapter8
+{
+    public class SampleData
+    {
+        public static void Initialize(MobileContext context)
+        {
+            if (!context.Phones.Any())
+            {
+                context.Phones.AddRange(
+                    new Phone
+                    {
+                        Name = "Iphone X",
+                        Company = "Apple",
+                        Price = 600
+                    },
+            new Phone
+            {
+                Name = "Xiaomi Mi A2",
+                Company = "Xiaomi",
+                Price = 200
+            },
+             new Phone
+             {
+                 Name = "Samsung Galaxy Edge",
+                 Company = "Samsung",
+                 Price = 550
+             }
+             );
+            }
+            context.SaveChanges();
+        }
+    }
+}
