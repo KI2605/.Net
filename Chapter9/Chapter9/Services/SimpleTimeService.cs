bb155483bf77d58b8d55f57dd54129c53ebddd72
Ihor Kolopatin <ikolopatin@gmail.com>
2021-04-21 18:56:56 +0300

    chapter9

---
+++ Chapter9/Chapter9/Services/SimpleTimeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter9.Services
+{
+    public class SimpleTimeService : ITimeServices
+    {
+        public SimpleTimeService()
+        {
+            Time = DateTime.Now.ToString("hh:mm:ss");
+        }
+        public string Time { get; }
+    }
+}
--- Chapter9/Chapter9/Startup.cs
+++ Chapter9/Chapter9/Startup.cs
@@ -1,3 +1,4 @@
+using Chapter9.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,8 @@ namespace Chapter9
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ITimeServices, SimpleTimeService>();
+        
             services.AddControllersWithViews();
         }
 
