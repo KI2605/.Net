7128dfade882cebda87312be3fe0ab822c66fb0e
Ihor Kolopatin <ikolopatin@gmail.com>
2021-04-27 11:24:44 +0300

    serveruser

--- ServerUser/ServerUser/Models/User.cs
+++ ServerUser/ServerUser/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -12,6 +13,7 @@ namespace ServerUser.Models
         [Key]
         public string NickName { get; set; }
         public string Password { get; set; }
+        [BindNever]
         public bool IsAdmin { get;set; }
     }
 }
