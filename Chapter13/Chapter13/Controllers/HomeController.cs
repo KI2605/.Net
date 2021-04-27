1aa24c55e0219904c486abfb97cff18f34d9af4b
Ihor Kolopatin <ikolopatin@gmail.com>
2021-04-27 11:28:59 +0300

    chapter13

---
+++ Chapter13/Chapter13/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Chapter13.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string name, int age)
+        {
+            return Content($"{name} - {age}");
+        }
+        public IActionResult GetUser()
+        {
+            return View();
+        }
+    }
+   
+}
