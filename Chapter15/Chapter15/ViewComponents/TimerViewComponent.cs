41940528296af71519fbcf1cc9ea58c6ae92c53e
Ihor Kolopatin <ikolopatin@gmail.com>
2021-04-29 19:14:34 +0300

    here is chapter 15 and its problems

---
+++ Chapter15/Chapter15/ViewComponents/TimerViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter15.Components
+{
+    public class Timer:ViewComponent
+    {
+        public string Invoke()
+        {
+            return $"Текущее время: {DateTime.Now.ToString("HH:mm:ss")}";
+        }
+    }
+}
